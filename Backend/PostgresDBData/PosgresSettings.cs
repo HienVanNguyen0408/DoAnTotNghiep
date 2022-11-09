f99ebb21606cd851ed7fa6642936aa4b77e18255
HienVanNguyen <hien.van.nguyen0408@gmail.com>
2022-11-09 18:38:52 +0700

    update

---
+++ Backend/PostgresDBData/PosgresSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostgresDBData
+{
+    public class PosgresSettings
+    {
+        public const string NAME = "PosgresSettings";
+
+        public string ConnectionString { get; set; } = "User ID=nvhien;Password=nvhien;Host=localhost;Port=5432;Database=postgres;";
+    }
+}
---
+++ Backend/PostgresDBData/PostgresDBContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace PostgresDBData
+{
+    public class PostgreSqlContext : DbContext
+    {
+        public PostgreSqlContext(DbContextOptions<PostgreSqlContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Patient> patients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public override int SaveChanges()
+        {
+            ChangeTracker.DetectChanges();
+            return base.SaveChanges();
+        }
+    }
+}
