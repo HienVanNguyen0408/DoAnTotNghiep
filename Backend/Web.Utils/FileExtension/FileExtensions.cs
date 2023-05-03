32f0582c361b0a450bada8cda5754764f4ebc72c
nvhien <hien.van.nguyen0408@gmail.com>
2023-05-03 13:15:57 +0700

    refactor code

--- Backend/Web.Utils/FileExtension/FileExtensions.cs
+++ Backend/Web.Utils/FileExtension/FileExtensions.cs
@@ -14,31 +14,22 @@ namespace Web.Utils
     {
         public static string GetFileExtention(FileType fileType)
         {
-            var extension = string.Empty;
-            switch (fileType)
-            {
-                case FileType.Image:
-                    extension = ".png";
-                    break;
-                default:
-                    extension = ".png";
-                    break;
-            }
+            var extension = ".png";
             return extension;
         }
-        
+
         public static void CreateFolder(string path) => Directory.CreateDirectory(path);
-        
+
         public static byte[] GetDataFile(string path) => File.ReadAllBytes(path);
-        
+
         public static bool CheckFolderExist(string path) => Directory.Exists(path);
 
         public static string GetFileNameByPathProduct(string path) => path.ReplaceText("product/", "");
-        
+
         public static string GetFileNameByPathBlog(string path) => path.ReplaceText("blog/", "");
-        
+
         public static string GetPathDirtoryCurrent() => Directory.GetCurrentDirectory();
-        
+
         public static string GetPathProductLocalClient()
         {
             var path = $"{Directory.GetCurrentDirectory()}";
@@ -52,9 +43,9 @@ namespace Web.Utils
             var pathClient = path.RegexMatch("(\\S+DoAnTotNghiep)").GroupText(1);
             return $"{pathClient}\\Fontend\\LocalStorage\\Blog";
         }
-        
+
         public static string GetPathProductLocal() => $"{Directory.GetCurrentDirectory()}\\LocalStorage\\Product";
-        
+
         public static string GetPathBlogLocal() => $"{Directory.GetCurrentDirectory()}\\LocalStorage\\Blog";
 
     }
